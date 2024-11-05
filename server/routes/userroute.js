0d0e60a1dcd77e68ddcd9db98a1cdc1e1cb195a9
Meinard Legashki Limlengco <152501369+Meliorasimp@users.noreply.github.com>
2024-11-05 18:32:56 +0800

    Delete server directory

--- server/routes/userroute.js
+++
@@ -1,9 +0,0 @@
-import { postuser, loginuser } from "../controller/usercontroller.js";
-import express from 'express';
-const router = express.Router();
-
-router.post('/register', postuser);
-router.post('/login', loginuser);
-
-export default router;
-
